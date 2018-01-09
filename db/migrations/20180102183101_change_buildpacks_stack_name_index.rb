5a4ef52b2b1dde88e3a6deecdaa74706d92a3c29
Sam Coward <scoward@pivotal.io>
2018-01-08 20:17:41 -0500

    index -> add_index

--- db/migrations/20180102183101_change_buildpacks_stack_name_index.rb
+++ db/migrations/20180102183101_change_buildpacks_stack_name_index.rb
@@ -2,14 +2,14 @@ Sequel.migration do
   up do
     alter_table(:buildpacks) do
       drop_index :name, unique: true
-      index [:name, :stack], unique: true
+      add_index [:name, :stack], unique: true
     end
   end
 
   down do
     alter_table(:buildpacks) do
       drop_index [:name, :stack], unique: true
-      index :name, unique: true
+      add_index :name, unique: true
     end
   end
 end
