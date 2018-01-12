741aba931b244ee0fa9780461ead51c843704183
Sam Coward <scoward@pivotal.io>
2018-01-12 10:17:03 -0500

    Combine migrations; get default buildpack stack from env

--- db/migrations/20180102183100_add_stack_to_buildpack_table.rb
+++ db/migrations/20180102183100_add_stack_to_buildpack_table.rb
@@ -1,12 +1,15 @@
 ## FIXME: Should this have been stack_id instead?
+require 'yaml'
 Sequel.migration do
   up do
     alter_table(:buildpacks) do
       add_column :stack, String, size: 255, null: true
+      drop_index :name, unique: true
+      add_index [:name, :stack], unique: true
     end
 
     ## FIXME: use default stack instead (could not work out how at this point)
-    self['UPDATE buildpacks SET stack = ?', 'cflinuxfs2'].update
+    self['UPDATE buildpacks SET stack = ?', default_stack].update
 
     alter_table(:buildpacks) do
       set_column_not_null :stack
@@ -15,7 +18,15 @@ Sequel.migration do
 
   down do
     alter_table(:buildpacks) do
+      drop_index [:name, :stack], unique: true
       drop_column :stack
+      add_index :name, unique: true
     end
+  end
+
+  def default_stack
+    stacks_yml_path = ENV.fetch("STACKS_YML", nil)
+    return YAML.load(File.read("/tmp/stacks.yml"))["default"] if stacks_yml_path && File.exist?(stacks_yml_path)
+    'unknown'
   end
 end
