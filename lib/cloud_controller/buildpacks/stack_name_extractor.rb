eecf0024df75532703958d76b938f2720678e5ab
Leah Hanson <lhanson@pivotal.io>
2018-03-27 16:24:06 -0400

    wip; Use zipruby instead
    Signed-off-by: Sam Coward <scoward@pivotal.io>
    Signed-off-by: Leah Hanson <lhanson@pivotal.io>

--- lib/cloud_controller/buildpacks/stack_name_extractor.rb
+++ lib/cloud_controller/buildpacks/stack_name_extractor.rb
@@ -1,3 +1,4 @@
+require 'zipruby'
 module VCAP::CloudController
   module Buildpacks
     class StackNameExtractor
@@ -5,18 +6,29 @@ module VCAP::CloudController
 
       def self.extract_from_file(bits_file_path)
         bits_file_path = bits_file_path.path if bits_file_path.respond_to?(:path)
-        Zip::File.open(bits_file_path) do |zip_file|
-          zip_file.each do |entry|
-            if entry.name == 'manifest.yml'
-              raise CloudController::Errors::BuildpackError.new('buildpack manifest is too large') if entry.size > ONE_MEGABYTE
-              return YAML.safe_load(entry.get_input_stream.read).dig('stack')
+        ZipRuby::Archive.open(bits_file_path) do |archive|
+          pos = archive.locate_name("manifest.yml")
+          if pos != -1
+            entry = archive.get_stat(pos)
+            raise CloudController::Errors::BuildpackError.new('buildpack manifest is too large') if entry.size > ONE_MEGABYTE
+            archive.fopen(pos) do |f|
+              return YAML.safe_load(f.read).dig("stack")
             end
           end
         end
+
+
+        #   zip_file.each do |entry|
+        #     if entry.name == 'manifest.yml'
+        #       raise CloudController::Errors::BuildpackError.new('buildpack manifest is too large') if entry.size > ONE_MEGABYTE
+        #       return YAML.safe_load(entry.get_input_stream.read).dig('stack')
+        #     end
+        #   end
+        # end
         nil
       rescue Psych::Exception
         raise CloudController::Errors::BuildpackError.new('buildpack manifest is not valid')
-      rescue Zip::Error
+      rescue ZipRuby::Error
         raise CloudController::Errors::BuildpackError.new('buildpack zipfile is not valid')
       end
     end
