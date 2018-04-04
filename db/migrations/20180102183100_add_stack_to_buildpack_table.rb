ceb6418f7c0d8a317788016605c2f8c3789d92de
Leah Hanson <lhanson@pivotal.io>
2018-04-04 11:32:13 -0400

    Migrate stacks based on stacks.yml contents, accounting for hwc_buildpack and windows stacks
    Signed-off-by: Sam Coward <scoward@pivotal.io>

--- db/migrations/20180102183100_add_stack_to_buildpack_table.rb
+++ db/migrations/20180102183100_add_stack_to_buildpack_table.rb
@@ -1,8 +1,24 @@
 require 'yaml'
 
-def default_stack
+def stacks_yml
   stacks_yml_path = ENV.fetch("STACKS_YML", nil)
-  YAML.load(File.read(stacks_yml_path))["default"] if stacks_yml_path && File.exist?(stacks_yml_path)
+  YAML.load(File.read(stacks_yml_path)) if stacks_yml_path && File.exist?(stacks_yml_path)
+end
+
+def default_stack
+  stacks_yml["default"] if stacks_yml
+end
+
+def latest_windows_stack
+  return unless stacks_yml
+  stack_names = stacks_yml["stacks"].map { |stack| stack["name"] }
+  if stack_names.include?("windows2016")
+    return "windows2016"
+  end
+  if stack_names.include?("windows2012R2")
+    return "windows2012R2"
+  end
+  return nil
 end
 
 Sequel.migration do
@@ -12,7 +28,8 @@ Sequel.migration do
       drop_index :name, unique: true
       add_index [:name, :stack], unique: true, name: :unique_name_and_stack
     end
-    self[:buildpacks].update(:stack => default_stack) if default_stack
+    self[:buildpacks].where(Sequel.negate(name: "hwc_buildpack")).update(stack: default_stack) if default_stack
+    self[:buildpacks].where(name: "hwc_buildpack").update(stack: latest_windows_stack) if latest_windows_stack
   end
 
   down do
