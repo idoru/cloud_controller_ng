1d2cd60812633a930ff578bc88a430fe59140206
Lisa Cho <lcho@pivotal.io>
2018-04-30 17:05:04 -0700

    wip - remove data migration and fix errors

--- app/fetchers/buildpack_lifecycle_fetcher.rb
+++ app/fetchers/buildpack_lifecycle_fetcher.rb
@@ -16,7 +16,10 @@ module VCAP::CloudController
       ##       to the user will probably be VERY confusing
 
       def ordered_buildpacks(buildpack_names, stack_name)
-        buildpacks = Buildpack.where(name: buildpack_names, stack: stack_name).all
+        buildpacks = Buildpack.where(name: buildpack_names, stack: stack_name).all #[]
+        # Given that some of these buildpacks have nil stack
+        # Return buildpacks that have nil stacks
+        # Except don't return them if they are a duplicate name of a buildpack that has a stack, and matching the stack_name
 
         buildpack_names.map do |buildpack_name|
           buildpack_record = buildpacks.find { |b| b.name == buildpack_name }
